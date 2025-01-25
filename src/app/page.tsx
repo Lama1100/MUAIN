ad8c144ba205b485d40f8ebc18e0e1f1e24f8a67
Lama <Lamasultan1100@gmail.com>
2025-01-25 20:19:56 +0300

    Initial commit with Next.js code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,23 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-   <div></div>
+    <div style={styles.container}>
+      <div style={styles.text}>مرحبا بك في معين</div>
+    </div>
   );
 }
+
+const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh', // Full viewport height
+    backgroundColor: '#f0f0f0', // Optional background color
+  },
+  text: {
+    fontSize: '36px', // Adjust font size as needed
+    fontFamily: '"Arial", sans-serif', // Change the font family
+    textAlign: 'center',
+  },
+};
